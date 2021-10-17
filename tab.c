5b4a159bf62a6ae6a922807097be3b1f1e0d5711
hazmat <ebertrand@gaming.tech>
2021-10-17 20:30:11 +0200

    commencement du tableau

---
+++ tab.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define NBL 6
+#define NBC 7
+#define cha x
+
+char tab[NBL][NBC];
+int main(void){
+  tab[NBC][NBL] = 'x';
+}    
+
+int init(void){ 
+  printf("%c", main());
+}
