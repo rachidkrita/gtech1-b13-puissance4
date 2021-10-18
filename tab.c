d7865d7e1c91558466e1459c7ce7c7d09dc29e90
Hazmat11 <ebertrand@gaming.tech>
2021-10-18 11:38:13 +0200

    début d'un truc merdique

--- tab.c
+++ tab.c
@@ -18,3 +18,22 @@ int main(void){
   }
   printf("+ 1-2-3-4-5-6-7 + \n");
 }    
+
+int li;
+char p1, p2;
+int choice;
+char player;
+int choose(void){
+  printf("Donnez un chiffre correspondant à une colonne");
+  scanf("choice");
+  if choise != int && choise <= 7{
+      printf("Veuillez rentrer un chiffre entier compris entre 1 et 7");
+    }
+  {
+  else printf(p1 || p2, game[choice]}
+
+ while li = p1 || p2{
+   do li = (l=6; l--)}
+ NBL = p1 || p2
+
+	      
