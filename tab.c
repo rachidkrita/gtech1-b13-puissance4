b0fe40cd0b05f6f2ed1f0e78bf35b7741e8d0d41
Karma192 <agrandpaul@gaming.tech>
2021-10-18 10:26:59 +0200

    Ajout de la boucle de jeu

--- tab.c
+++ tab.c
@@ -2,6 +2,9 @@
 #define NBL 6
 #define NBC 7
 #define cha x
+#define BOOL char
+#define FALSE 0
+#define TRUE 1
 
 char tab[NBL][NBC];
 int main(void){
@@ -11,3 +14,11 @@ int main(void){
 int init(void){ 
   printf("%c", main());
 }
+
+int Game(void) {
+  while OnOff != 1 {
+      
+      while Victory != 1 || AccountRound < 42 {
+      
+      choice();
+      
