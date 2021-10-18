778439c42c508b5c04b672bfcbbab6a57c72e301
Hazmat11 <ebertrand@gaming.tech>
2021-10-18 10:21:06 +0200

    le tableau marche

--- tab.c
+++ tab.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 #define NBL 6
 #define NBC 7
-#define cha x
 
-char tab[NBL][NBC];
+int c, l;
 int main(void){
-  tab[NBC][NBL] = 'x';
+  char game[NBL][NBC];
+  game[NBL][NBC] = '.';
+  printf("\n");
+  printf("+ - - - - - - - + \n");
+  for (l=0; l<NBL; l++){
+    printf("|");
+    
+    for (c=0; c<NBC; c++){
+      printf (" %c", game[NBL][NBC]);
+    }
+    printf(" |\n");
+  }
+  printf("+ 1-2-3-4-5-6-7 + \n");
 }    
-
-int init(void){ 
-  printf("%c", main());
-}
