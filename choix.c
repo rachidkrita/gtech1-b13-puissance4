58f1b41b1416deaf38b1ba806720d5193d361797
Kyosuuke <92298804+Kyosuuke@users.noreply.github.com>
2021-10-19 15:01:49 +0200

    Update choix.c

--- choix.c
+++ choix.c
@@ -23,26 +23,3 @@ int main ()
     
     return 0;
 }
-=======
-int main ()
-{
-  int chosennumber = 0;
-  printf ("Welcome in Puissance 4!\n") && printf ("Please choose your sign.\n");
-
-  do
- 
-  {
-    char token [] = "ox";
-    scanf("%d",&chosennumber);
-
-    if (chosennumber == 1)
-      printf("You are player 1 : Your sign is o!\n");
-    else if (chosennumber == 2)
-      printf("You are player 2 : Your sign is x!\n");
-    else
-      printf("Please choose a number between 1 and 2.\n");
-  } while (chosennumber != 1&&2);
-
-  return 0;
-    }
->>>>>>> 0bbf54c627049d2cac040cb85d8160365b5268a2
