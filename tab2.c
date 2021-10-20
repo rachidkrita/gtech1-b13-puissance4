17363181cf21c0a09f3d5600c44bb2f306c40226
Hazmat11 <ebertrand@gaming.tech>
2021-10-20 17:09:12 +0200

    okay on essaye de mélanger

--- tab2.c
+++ tab2.c
@@ -6,6 +6,8 @@ int c,l;
 int choice;
 char game[NBL][NBC];
 char x;
+char token[] = "ox";
+int joueur = 0; // Joueur 1 = 0, Joueur 2 =1
 
 //def de la fonction initiant le tab
 void initTab(){
@@ -41,6 +43,21 @@ void choose(){
   printf("+ 1-2-3-4-5-6-7 + \n");
 }    
 
+void loop(){
+  int Victory = 0;
+  int OnOff = 0;
+  int AccountRound = 1;
+  while (OnOff != 1) {
+    OnOff == 0;
+    token[joueur];
+    printf("Bienvenue au jeu du Puissance 4 \n\n");
+    while (Victory != 1 || AccountRound < 42) {
+      Victory == 1;
+      OnOff == 1;
+      joueur = !joueur;
+    }
+  }
+}
 				     
 //fonction principale avec le tableau et le choix
 int main(void){
