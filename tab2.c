808823d9b541fc1c498328da7afad65f0bbbb68f
Hazmat11 <ebertrand@gaming.tech>
2021-10-27 13:56:05 +0200

    logo stylé

--- tab2.c
+++ tab2.c
@@ -1,4 +1,4 @@
-//définition de variables
+//définition de plein de trucs
 #include <stdio.h>
 #define NBL 6
 #define NBC 7
@@ -10,100 +10,131 @@ int AccountRound = 1;
 int next;
 char token[] = "ox";
 int player = 0; // Joueur 1 = 0, Joueur 2 =1
-
-int logo() {
-  printf(R"EOF(
-	       _________      .__                                               _____  
-	       \______  \__ __|__|______ ____________    ____   ____  ____     /  |  | 
-	       |     ___/  |  \  |/  ___/   ___/\__  \  /    \_/ ___\/ __ \   /   |  |_
-	       |    |   |  |  /  |\___ \ \___ \  / __ \|   |  \  \__\  ___/  /    ^   /
-	       |____|   |____/|__/____  >____  >/____  /___|  /\___  >___  > \____   | 
-				      \/     \/      \/     \/     \/    \/       |__| 
-	 )EOF");
-}                
+int good = 0;
 
 //de belles couleurs :)))))))
-void red() {
+void red(){
   printf("\033[1;31m");
 }
-void yellow() {
+void yellow(){
   printf("\033[1;33m");
 }
-void white() {
+void white(){
   printf("\033[0;37m");
 }
-void reset() {
+void reset(){
   printf("\033[0m");
 }
+void blue(){
+  printf("\033[0;34m");
+}
 
-//fonction initialisant le tableau
-void InitTab() {
-  for (l=0; l<NBL; l++) {
-      for (c=0; c<NBC; c++) {
-	game[l][c]='.';
+//logo trop styléééééééééééé
+int logo() {
+  blue();
+  printf(R"EOF(
+            	         _________      .__                                               _____
+		         \______  \__ __|__|______ ____________    ____   ____  ____     /  |  |
+		         |     ___/  |  \  |/  ___/   ___/\__  \  /    \_/ ___\/ __ \   /   |  |_
+          	         |    |   |  |  /  |\___ \ \___ \  / __ \|   |  \  \__\  ___/  /    ^   /
+	                 |____|   |____/|__/____  >____  >/____  /___|  /\___  >___  > \____   |
+		                                \/     \/      \/     \/     \/    \/       |__|
+	       )EOF");
+  reset();
+}  
+
+//fonction initialisant le tab
+void InitTab(){
+  for (l=0; l<NBL; l++){
+    for (c=0; c<NBC; c++){
+      game[l][c]='.';
     }
   }
 }
 
-//fonction affichant le tableau
-void PrintTab() {
+//empty le journal d'entrée
+void flushstdin() {
+  int c;
+  while((c = getchar()) != '\n' && c != EOF);}
+
+//def de la fonction affichant le tab
+void PrintTab(){
   printf("\n");
   printf("+ - - - - - - - + \n");
-  for (l=0; l<NBL; l++) {
+  for (l=0; l<NBL; l++){
     printf("|");
-    for (c=0; c<NBC; c++) {
-  printf(" %c", game[l][c]);
+    for (c=0; c<NBC; c++){
+      printf(" %c", game[l][c]);
     }
-  printf(" |\n");
+    printf(" |\n");
   }
   printf("+ 1-2-3-4-5-6-7 + \n");
 }
 
-int Verif() { //Fonction pour verifier les places disponibles dans la colonne choisie
+//Fonction permettant d'empiler les jetons
+int Verif() {
   int exit = 0;
   next = 0;
-  while (next < 5) {
+  while (exit != 1 && next < 5) {
     if (game[next][choice] == '.') {
       next ++;
+      exit = 0;
     }
     if (game[next][choice] == 'x' || game[next][choice] == 'o') {
-	if (next == 0) {
-	  printf("Cette colonne est pleine...");
-	  return 0;
-	}
-	else {
-	  next --;
-	  return 1;
-	}
+      if (next == 0) {
+	red();
+	printf("\nCette colonne est pleine...\n");
+	reset();
+	exit ++;
+	player=!player;
+	AccountRound--;
+	return 0;
+      }
+      else {
+	next --;
+	exit ++;
+	return 1;
+      }
     }
   }
 }
 
-
-//fonction principale du jeu
-void choose() {
+//fonction pour choisir
+int choose(){
+  int scanerror;
   printf("\n");
   while (AccountRound < 42) {
+    int good = 0;
     PrintTab();
+    yellow();
     printf("\nVotre chiffre: ");
-    scanf("%d", &choice);
-    choice --;
-    if (choice < 1 || choice > 7) {
+    reset();
+    scanerror=scanf("%d", &choice);
+    choice--;
+    if (scanerror==0){
+      flushstdin();
       red();
-      printf("\nVotre chiffre ne corresponds pas à une colonne \n");
+      printf("\nCe n'est pas un chiffre :(\n");
+      reset();
+      continue;
+    }
+    if (choice <= -1 || choice >= 7){
+      red();
+      printf("\nVeuillez mettre un chiffre entre 1 et 7");
       reset();
       choose();
     }
-    if (Verif() == 1) {
-      game[next][choice] = token[player];
+    good = Verif();
+    if (good = 1) {
+      game[next][choice]= token[player];
       player = !player;
-      AccountRound ++;
+      AccountRound++;
     }
   }
 }
 
-//fonction main
-int main(void) {
+//fonction principale avec le tableau et le choix
+int main(void){
   logo();
   InitTab();
   yellow();
